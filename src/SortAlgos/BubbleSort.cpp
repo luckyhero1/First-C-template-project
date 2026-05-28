fa0deeb098b61cb7bba86df59f29ad78f20e31bb
Michael <158811940+luckyhero1@users.noreply.github.com>
2026-05-28 10:19:51 +0200

    Update BubbleSort.cpp

--- src/SortAlgos/BubbleSort.cpp
+++ src/SortAlgos/BubbleSort.cpp
@@ -2,38 +2,50 @@
 #include <fstream>
 #include <cmath>
 
+int give_rgb(int value, int c0, int c1)
+{
+    // Diese Funktion soll zwischen den Werten im Spektrum pendeln.
+    // c(x) = (1−t(x))*c0​ + t(x)*c1​ mit t(x) = (1−cos(((2*pi)/P) * x)​)/2 und c0 Startwert RGB und c1 Endwert
+
+    double tx = (1 - cos(((2 * M_PI) / 20) * value)) / 2.0;
+    return c0 * (1 - tx) + c1 * tx;
+}
+
 template<typename T>
 void show_obj(T& obj, std::ostream& out, int pivot_element = 0, int value = 0, int was_sorted_flag = 0) {
     out << "\\begin{tikzpicture}\n"
-            "\n";
+            "\\node[anchor = west] at (0, 0.5) {Schritt: " << value + 1 << "};\n"
+            "\\draw[step = 1, gray!10](0, -1) grid(" << obj.size() - 0.5 << ", 1);\n";
 
-    //int color = 127 * sin(0.15 * value - M_PI / 2) + 128;
-    //int color = 40 * (M_PI / 2.0) * asin(sin((2 * value) - (0.5 * M_PI))) + 1;
-    int color = ((sin(0.25 * value - 1.5) + 1) / 2.0) * 50.0;
+    // int color = ((sin(0.25 * value - 1.5) + 1) / 2.0) * 50.0;
+    int rgb_red = give_rgb(value, 240, 100);
+    int rgb_green = give_rgb(value, 245, 141);
 
     for (int i = 0; i < obj.size(); i++)
     {
+        int column = i; // sollte +2 sein
         if(i == pivot_element)
         {
-            out << "\\node at (" << i << ",0) [behind path, fill=red!40] {" << obj[i]->value << "};\n";
+            out << "\\node at (" << column << ",0) [behind path, fill=red!40] {" << obj[i]->value << "};\n";
         }
         else if ((i - 1 == pivot_element) && (i != obj.size()) && (value != 0))
         {
-            out << "\\node at (" << i << ",0) [behind path, fill=orange!60] {" << obj[i]->value << "};\n";
+            out << "\\node at (" << column << ",0) [behind path, fill=orange!60] {" << obj[i]->value << "};\n";
         }
         else if ((i + 1 == pivot_element) && (was_sorted_flag == 1) && (value != 0))
         {
-            out << "\\node at (" << i << ",0) [behind path, fill=green!60] {" << obj[i]->value << "};\n";
+            out << "\\node at (" << column << ",0) [behind path, fill=green!60] {" << obj[i]->value << "};\n";
         }
         else
         {
-            out << "\\node at (" << i << ",0) [behind path, fill=blue!" << color << "] {" << obj[i]->value << "};\n";
+            out << "\\node at (" << column << ",0) [behind path, fill={rgb,255:red," << rgb_red << "; green," << rgb_green << "; blue,255}] {" << obj[i]->value << "};\n";
         }
     }
 
-    out <<  "\n"
-            "\\end{tikzpicture}\n"
-            "\n";
+    out << "\n"
+           "\\draw[gray!30] (1.5,-0.5) rectangle (" << 1.5 + obj.size() << ",0.5);\n"
+           "\\end{tikzpicture}\n"
+           "\n";
 }
 
 template<typename T>
