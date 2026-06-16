9a85ff71acdfb75cd6a3d5144348ed4fa334a161
Michael <158811940+luckyhero1@users.noreply.github.com>
2026-06-16 15:00:24 +0200

    Updated README.md
    Updated file and made BubbleSort.hpp clean. Next step is to modify the sort-function and list to work with the iterators.

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# First C++-template project
-## Summary
-The goal of this library is to implement some data structures and use some sort algorithms with visualization.
+# Visualization of the sorting of a data structure using LaTeX
+
+This application instantiates doubly-linked lists, stores random values ​​in them and sorts them using Bubble-Sort.
+The steps of the sorting algorithm are then written to a TeX-file for each list.
+The TeX-files are converted into PDF files.
+
+## How to use:
+
+### Prerequisites
+
+You need CMake (version >= 3.20) and Latexmk (install a perl-environment if your TeX-distribution does not already have one).
+
+### Using Visual Studio Code
+
+Press F7 and let CMake select the correct compiler.
+
+### Using Terminal
+
+Use the following commands at the root of the directory.
+
+```
+cmake -B build -S .
+cmake --build build
+```
+
+## How it's made:
+
+**Resources:** C++ (Standardlibs), CMake, LaTeX
+
+**Sources/Aids:** ...
+
+Hier könnte Ihr Text stehen!
+
+### DoublyLinkedList.hpp
+
+Hier könnte Ihr Text stehen!
+
+### BubbleSort.hpp
+
+Hier könnte Ihr Text stehen!
+
+### LatexDocument.hpp
+
+The struct LatexDocument defines the format of the TeX-File. It saves an output stream, which will be written into the TeX-File. The sorting steps will be written into this LatexDocument. 
+
+### main.cpp
+
+This file serves as an interface. Here you can create lists und LatexDocuments and call the Bubble-Sort function.
+
+## What did I learn:
+
+Hier könnte Ihr Text stehen!
--- src/SortAlgos/BubbleSort.hpp
+++ src/SortAlgos/BubbleSort.hpp
@@ -2,100 +2,79 @@
 
 #include <iostream>
 #include <fstream>
-#include <cmath>
 #include <string>
 
-int give_rgb(int value, int c0, int c1)
-{
-    // Diese Funktion soll zwischen den Werten im Spektrum pendeln.
-    // c(x) = (1−t(x))*c0​ + t(x)*c1​ mit t(x) = (1−cos(((2*pi)/P) * x)​)/2 und c0 Startwert RGB und c1 Endwert
-
-    double tx = (1 - cos(((2 * M_PI) / 20) * value)) / 2.0;
-    return c0 * (1 - tx) + c1 * tx;
-}
-
+/**
+ * @brief
+ * @param obj              : Object you want to sort (it needs a 'int size()'-function)
+ * @param out              : Output stream you want to write the visualization into
+ * @param pivot_element    : Number of the current element to sort
+ * @param value            : Counter of function calls
+ * @param was_sorted_flag  : This flag is 1, if the current compare results in a swap
+ */
 template<typename T>
 void show_obj(T& obj, std::ostream& out, int pivot_element = 0, int value = 0, int was_sorted_flag = 0) {
     out << "\\begin{tikzpicture}[x=1cm, y=1cm]\n"
            "\\node[anchor = west] at (-0.6, 0.8) {Schritt: " << value + 1 << "};\n"
            "\n";
 
-    /*for (int i = 0; i < obj.size(); i++)
-    {
-        int column = i; // sollte +2 sein
-        if(i == pivot_element)
-        {
-            out << "\\node at (" << column << ",0) [behind path, fill=red!40] {" << obj[i]->value << "};\n";
-        }
-        else if ((i - 1 == pivot_element) && (i != obj.size()) && (value != 0))
-        {
-            out << "\\node at (" << column << ",0) [behind path, fill=orange!60] {" << obj[i]->value << "};\n";
-        }
-        else if ((i + 1 == pivot_element) && (was_sorted_flag == 1) && (value != 0))
-        {
-            out << "\\node at (" << column << ",0) [behind path, fill=green!60] {" << obj[i]->value << "};\n";
-        }
-        else
-        {
-            out << "\\node at (" << column << ",0) [behind path, fill={rgb,255:red," << rgb_red << "; green," << rgb_green << "; blue,255}] {" << obj[i]->value << "};\n";
-        }
-    }
-
-    out << "\n"
-           "\\draw[gray!30] (1.5,-0.5) rectangle (" << 1.5 + obj.size() << ",0.5);\n"
-           "\\end{tikzpicture}\n"
-           "\n";*/
-
     int row = 0;
     for (int i = 0; i < obj.size(); i++)
     {
-        std::string color;
-        if((i != 0) && (i % 17 == 0))
+        if((i != 0) && (i % 17 == 0))   // For more than 16 elements in a row -> create new line
         {
             row -= 1;
         }
 
+        std::string color;
         if (i == pivot_element) // Draws the current "bubble-element"/iterator
         {
             color = "red!60";
         }
-        else if (i - 1 == pivot_element && was_sorted_flag == 0 && value != 0) // next element, that will be compared to current
+        else if (i - 1 == pivot_element && was_sorted_flag == 0 && value != 0)  // Next element, that will be compared to current
         {
             color = "orange!60";
         }
-        else if (i + 1 == pivot_element && was_sorted_flag == 1 && value != 0)
+        else if (i + 1 == pivot_element && was_sorted_flag == 1 && value != 0)  // Element, that was swapped
         {
             color = "green!60";
         }
         else
         {
-            color = "{rgb,255:red,191; green,209; blue,255}";
+            color = "{rgb,255:red,191; green,209; blue,255}";   // Default color for every other node
         }
 
         out << "\\node[draw, minimum width=0.8cm, minimum height=0.8cm, fill=" << color
-            << "] at (" << i % 17 << "," << row << ") {" << obj[i]->value << "};\n"
-            "\n";
+            << "] at (" << i % 17 << "," << row << ") {" << obj[i]->value << "};\n";
     }
 
-    out << "\\end{tikzpicture}\n"
+    out <<  "\n"
+            "\\end{tikzpicture}\n"
             "\n";
 }
 
+/**
+ * @brief
+ * @param obj              : Object you want to sort (it needs a 'int size()'-function)
+ * @param out              : Output stream you want to write the visualization into
+ */
 template<typename T>
 void bubble_sort(T& obj, std::ofstream& out)
 {
-    show_obj<T>(obj, out);
+    show_obj<T>(obj, out);  // Default list visualization with the 0. element as pivot element
+    
     int length = obj.size();
-    int pivot_element;
+    int pivot_element = 0;
     int count = 0;
 
+    // 
     for(int i = 1; i < length + 1; i += 1)
     {
         for(int j = 0; j < length - 1; j += 1)
         {
             pivot_element = j;
             count += 1;
-            show_obj<T>(obj, out, pivot_element, count);
+            show_obj<T>(obj, out, pivot_element, count);    // Shows the compare
 
             if(obj[j]->value > obj[j + 1]->value)
             {
@@ -104,7 +83,7 @@ void bubble_sort(T& obj, std::ofstream& out)
                 obj[j + 1]->value = obj[j]->value;
                 obj[j]->value = c;
                 count += 1;
-                show_obj<T>(obj, out, pivot_element, count, 1);
+                show_obj<T>(obj, out, pivot_element, count, 1); // Shows the switch
             }
         }
     }
